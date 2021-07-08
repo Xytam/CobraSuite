bae486a97b96c32f7f1b5fa0c74cfe5a1ab575c5
John Horvers <jhorvers97@gmail.com>
2021-07-08 17:15:26 -0400

    Added Bandwdith Utilization
    added new output

--- ec2instances/instanceneeds.py
+++ ec2instances/instanceneeds.py
@@ -13,7 +13,7 @@ def getInstanceNeeds(data_file):
     AZ = str(input("Enter desired AZ (us-east-1 default): ") or 'us-east-1')
     desiredMBpsIngress = int(input("Enter desired input in MegaBYTES per second(5 default)") or 5)
     desiredMBpsEgress = int(input("Enter desired output in MegaBYTES per second(defaults to previous answer eg 1:1 input/output)") or desiredMBpsIngress)
-    replicatdsadsad asfarevionFactor = int(input("Enter desired replication factor average (default 3)") or 3)
+    replicationFactor = int(input("Enter desired replication factor average (default 3)") or 3)
     instanceCount = int(input("Enter amount of brokers, this assumes you have enough partitions to evenly distribute load (default 5)") or 5)
 
 
@@ -30,14 +30,14 @@ def getInstanceNeeds(data_file):
             instanceData[i].update({'mbPerDollar': (instanceData[i]['ebs_baseline_bandwidth']*60*60)/float(instanceData[i]['pricing'][AZ]['linux']['ondemand'])})
 
             #This should dynamically calculate on frontend from js eventually
-            instanceData[i].update({'bandwidthUtilization': (((desiredMBpsIngress*replicationFactor/instanceCount) + (desiredMBpsEgress/instanceCount)) / float(instanceData[i]['ebs_baseline_throughput_mbps'] )})
+            instanceData[i].update({'bandwidthUtilization': ( ((desiredMBpsIngress*replicationFactor/instanceCount) + (desiredMBpsEgress/instanceCount)) / float(instanceData[i]['ebs_baseline_throughput_mbps'])) })
             #should calculate total annual cost to run and give option to sort annual operating cost vs "best value"
             instanceData[i].update({'totalCostToRun': float(instanceData[i]['pricing'][AZ]['linux']['ondemand'])*24*7*365})
             filtered.append(instanceData[i])
 
     sorted_data = sorted(filtered,key=lambda k: k['totalCostToRun'],reverse=False)
     for i in range(0,len(sorted_data)):
-        print("Name: ",sorted_data[i]['instance_type']," / mbPerDollar: ",sorted_data[i]['mbPerDollar']," / Total Anual Cost: ",sorted_data[i]['totalCostToRun'], " / EBS Bandwidth Utilization: " sorted_data[i]['bandwidthUtilization'] )
+        print("Name: ",sorted_data[i]['instance_type']," / mbPerDollar: ",sorted_data[i]['mbPerDollar']," / Total Anual Cost: ",sorted_data[i]['totalCostToRun'], " / EBS Bandwidth Utilization %: " , round(sorted_data[i]['bandwidthUtilization']*100,2) )
     print("Viable Instance Count: ",len(sorted_data))
 
 #            print("Instance Type: ",instance['instance_type']," Net Cost: ",instance['mbPerDollar'])
