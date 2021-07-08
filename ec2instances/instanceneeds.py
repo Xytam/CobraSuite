b0f4946eae3abfe0cd36510a7926ec84c2f4b4e4
John Horvers <jhorvers97@gmail.com>
2021-07-08 17:27:11 -0400

    Data Refeeding
    initial cut at data refeeding, will be reworked into producers/consumers like our sizing thing

--- ec2instances/instanceneeds.py
+++ ec2instances/instanceneeds.py
@@ -15,7 +15,7 @@ def getInstanceNeeds(data_file):
     desiredMBpsEgress = int(input("Enter desired output in MegaBYTES per second(defaults to previous answer eg 1:1 input/output)") or desiredMBpsIngress)
     replicationFactor = int(input("Enter desired replication factor average (default 3)") or 3)
     instanceCount = int(input("Enter amount of brokers, this assumes you have enough partitions to evenly distribute load (default 5)") or 5)
-
+    desiredRefeed = int(input("How much data are you re-feeding in MegaBYTES per second? (0 default)") or 0)
 
     with open(data_file, 'r') as f:
         instanceData = json.load(f)
@@ -23,8 +23,7 @@ def getInstanceNeeds(data_file):
     for i in range(0,len(instanceData)):
         #doing calculation of mbps here but may move it
         if(instanceData[i]['vCPU'] > 4 and instanceData[i]['memory'] > 32 and instanceData[i]['ebs_baseline_bandwidth'] > 0 and bool(instanceData[i]['pricing']) and
-        ((desiredMBpsIngress*replicationFactor/instanceCount) + (desiredMBpsEgress/instanceCount)) < (float(instanceData[i]['ebs_baseline_throughput_mbps']))):
-            #add flag to see if the instance should even be in the running
+        ((((desiredRefeed + desiredMBpsIngress)*replicationFactor) + (desiredMBpsEgress)) / instanceCount) < (float(instanceData[i]['ebs_baseline_throughput_mbps']))):
             #doesn't currently take memory to memory operations in account but should
             #should also take into account % fullness of bandwidth and maybe throw error if disk can't keep up because memory to memory ops are fine
             instanceData[i].update({'mbPerDollar': (instanceData[i]['ebs_baseline_bandwidth']*60*60)/float(instanceData[i]['pricing'][AZ]['linux']['ondemand'])})
@@ -40,8 +39,6 @@ def getInstanceNeeds(data_file):
         print("Name: ",sorted_data[i]['instance_type']," / mbPerDollar: ",sorted_data[i]['mbPerDollar']," / Total Anual Cost: ",sorted_data[i]['totalCostToRun'], " / EBS Bandwidth Utilization %: " , round(sorted_data[i]['bandwidthUtilization']*100,2) )
     print("Viable Instance Count: ",len(sorted_data))
 
-#            print("Instance Type: ",instance['instance_type']," Net Cost: ",instance['mbPerDollar'])
-#def calculateCost():
 
 if __name__ == '__main__':
     scrape('www/instances.json')
