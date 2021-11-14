b62cfef8f83d823a949368f02a948e70b4606085
Xytam <jhorvers97@gmail.com>
2021-11-14 12:42:14 -0500

    Added more content

--- releaseDownloader.sh
+++ releaseDownloader.sh
@@ -23,3 +23,15 @@ for i in "${resourceURLs[@]}"
 do
    wget -P connectors/ -N $i
 done
+
+echo "Pulling Jolokia and JMX"
+curl http://search.maven.org/remotecontent?filepath=org/jolokia/jolokia-jvm/1.6.2/jolokia-jvm-1.6.2-agent.jar -OJL
+curl https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.12.0/jmx_prometheus_javaagent-0.12.0.jar -OJL
+mkdir monitoringJARs/
+mv *.jar monitoringJARs/
+
+echo "Pulling Monitoring Samples"
+wget https://github.com/confluentinc/jmx-monitoring-stacks/archive/refs/heads/6.1.0-post.zip
+unzip 6.1.0-post.zip
+mv jmx-monitoring-stacks-6.1.0-post monitoringSamples
+rm 6.1.0-post.zip
